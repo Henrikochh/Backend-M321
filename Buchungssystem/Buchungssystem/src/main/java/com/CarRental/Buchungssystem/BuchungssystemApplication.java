6fd61b7d4e5ceb8fade38e743ea49aa1958d2bea
Henrikochh <113605484+Henrikochh@users.noreply.github.com>
2025-09-29 09:22:09 +0200

    Creating the spring boot application for Buchungssystem

---
+++ Buchungssystem/Buchungssystem/src/main/java/com/CarRental/Buchungssystem/BuchungssystemApplication.java
@@ -0,0 +1,13 @@
+package com.CarRental.Buchungssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchungssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchungssystemApplication.class, args);
+	}
+
+}
